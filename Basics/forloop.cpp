a8635155a22eb3fb5d55e51cb44c3b0d3ff5fda8
heyitsharman <harmankaur0779@gmail.com>
2024-12-23 10:15:16 +0530

    c

---
+++ Basics/forloop.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+// to print alphabets a to z
+// int main(){
+//    char name ;
+//     for(name = 'a'; name <='z';name++){
+//         cout << name;
+//     }
+//     return 0;
+// }
+
+// print numbers in reverse order
+// int main(){
+//     for(int i=10; i>=1; i--){
+//         cout << i << " ";
+//     }
+// }
+
+
+// multiplication table
+// int main(){
+//     int n;
+//     cout << "enter number:" << endl;
+//     cin >> n;
+//     for(int i=1; i<=10; i++){
+//         cout << n << " " << "x" << " " << i << " = " << n*i << endl;
+//     }
+//     return 0;
+// }
+
+
+// calculate power of a number 1st approach
+// int main(){
+//     int num, pow;
+//     cin >> num >> pow;
+//     int n = num;
+//     for(int i=1; i<pow; i++){
+//         num = num * n;
+//     }
+//     cout << num << endl;
+//     return 0;
+// }
+
+
+// sum of first n natural numbers
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     int sum =0;
+//     for(int i=0; i<=n; i++){
+//         sum +=i;
+//     }
+
+//     cout << sum ;
+//     return 0;
+// }
+
+
+// factorial of a number
+// int main(){
+//     int n, f= 1;
+//     cin >> n; 
+//     for(int i=1; i<=n; i++){
+//         f=f*i;
+//     }
+//     cout << f << endl;
+// }
+
+
+// check if a number is prime or not
+// int main(){
+//     int n;
+//     cin >> n;
+//     if(n<2){
+//         cout << "Not Prime" << endl;
+//         return 0;
+//     }
+//     else{
+//         for(int i=2; i<n; i++){
+//             if(n%i==0){
+//                 cout << "Not Prime" << endl;
+//                 return 0;
+//             }
+
+//         }
+//         cout << "Prime" << endl;
+//     }
+
+//     return 0;
+    
+// }
+
+ 
